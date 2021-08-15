3051bec74b480c999ca2d6b647f03e30bfe0c6d3
Gr0ki <87580615+Gr0ki@users.noreply.github.com>
2021-08-15 17:12:44 +0300

    Add files via upload

---
+++ Auto_reconnect_bluetooth_device.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+# Put the mac-address of the device you want to reconnect
+MAC_ADDRESS = ''
+
+def bluetooth_off():
+    _ = os.system('rfkill block bluetooth')
+
+def bluetooth_on():
+    _ = os.system('rfkill unblock bluetooth')
+
+def bluetooth_reboot():
+    bluetooth_off()
+    bluetooth_on()
+
+def remove_device():
+    command = 'bluetoothctl remove ' + MAC_ADDRESS
+    _ = os.system(command)
+
+def scan_on():
+    _ = os.system('bluetoothctl scan on')
+
+def pair_device():
+    command = 'bluetoothctl pair ' + MAC_ADDRESS
+    return os.system(command)
+
+def connect():
+    command = 'bluetoothctl connect ' + MAC_ADDRESS
+    _ = os.system(command)
+
+def set_as_trusted():
+    command = 'bluetoothctl trust ' + MAC_ADDRESS
+    _ = os.system(command)
+
+def notification():
+    _ = os.system('notify-send -t 3000 "Script" "I did my work here, check the result." --icon=/home/dr_xyz/Scripts/icons/172308_bluetooth_icon.png')
+
+def attempts_to_pair():
+    counter = 0
+    if pair_device() == 0 and counter <= 5:
+        return
+    else:
+        counter += 1
+        time.sleep(1)                   # Delay is for scanning (in child process)
+
+
+bluetooth_reboot()
+time.sleep(4)
+remove_device()
+
+pid = os.fork()                         # Fork is for ability to stop scanning for bluetooth devices
+child_pid = 0
+if pid > 0:
+    # Here is parent process
+    time.sleep(5)                       # Delay is for scanning (in child process)
+    attempts_to_pair()
+    connect()
+    set_as_trusted()
+    notification()
+    command = 'kill '+ str(child_pid)
+    os.system(command)
+else:
+    # Here is child process
+    child_pid = os.getpid()
+    scan_on()
